42a215a4892fad84b4c67ac2d5ff66450637d53d
Anthony McShane <a89th@hotmail.com>
2019-01-15 18:53:37 +0000

    javascript picks up quantities for all currencies to buy page

--- app/models/buy.rb
+++ app/models/buy.rb
@@ -4,11 +4,19 @@ class Buy < ApplicationRecord
 
 require 'httparty'
 
-def self.buy_price
+def self.btc_buy_price
   url = 'https://api.kraken.com/0/public/Ticker?pair=XXBTZEUR'
   response = HTTParty.get(url)
   response.parsed_response
   kraken_price = response.parsed_response["result"]["XXBTZEUR"]["c"][0].to_f
+  buy_price = (kraken_price * 1.025)
+end
+
+def self.eth_buy_price
+  url = 'https://api.kraken.com/0/public/Ticker?pair=XETHZEUR'
+  response = HTTParty.get(url)
+  response.parsed_response
+  kraken_price = response.parsed_response["result"]["XETHZEUR"]["c"][0].to_f
   buy_price = (kraken_price * 1.025)
 end
 
