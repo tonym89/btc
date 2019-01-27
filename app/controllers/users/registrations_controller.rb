d3237dae5a847605b57793101363b6f3b69fd491
Anthony McShane <a89th@hotmail.com>
2019-01-27 18:33:43 +0000

    first and last name added to user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,11 +13,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 protected
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :selfie, :document)
+    params.require(:user).permit(:email, :password, :password_confirmation, :selfie, :document, :first_name, :last_name)
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :selfie, :document, :current_password, :kyc_status)
+    params.require(:user).permit(:email, :password, :password_confirmation, :selfie, :document, :current_password, :kyc_status, :first_name, :last_name)
   end
 
 end
